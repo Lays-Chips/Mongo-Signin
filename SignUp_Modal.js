080d5ea28620bc6c43fe5986a765562dba769f5c
Jacob <62536091+Lays-Chips@users.noreply.github.com>
2021-10-21 21:20:56 -0700

    Add files via upload

---
+++ SignUp_Modal.js
@@ -0,0 +1,9 @@
+// Get the modal
+var modal = document.getElementById('SignUp_Modal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
